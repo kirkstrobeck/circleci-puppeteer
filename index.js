5a636a6dfd92b987e1c0c7ba7aecca37a6059bea
Kirk Strobeck <kirk@strobeck.com>
2019-03-09 09:01:27 -0800

    1

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const puppeteer = require('puppeteer');
 (async () => {
   mkdirp('./tmp');
 
-  const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    // executablePath:
+    //   '/home/sagar/workplace/scraping-demo/node_modules/puppeteer/.local-chromium/linux-599821/chrome-linux/chrome',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
   const page = await browser.newPage();
   await page.goto('https://www.google.com/');
 
